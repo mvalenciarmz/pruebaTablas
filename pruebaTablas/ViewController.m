e8bbbaae0c915226d72ba9fcb3f84a4857708ee1
Marcos <marcos@Marcoss-Mac-mini.local>
2015-01-29 17:54:27 -0600

    Initial Commit

---
+++ pruebaTablas/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pruebaTablas
+//
+//  Created by Marcos on 1/29/15.
+//  Copyright (c) 2015 Marcos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
