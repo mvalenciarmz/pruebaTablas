12bd4ff0e540c20fd2d7748fce952321f25deb2a
Marcos <marcos@Marcoss-Mac-mini.local>
2015-02-02 13:47:49 -0600

    prueba simple de tableview

---
+++ MyTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyTableViewController.m
+//  pruebaTablas
+//
+//  Created by Marcos on 1/29/15.
+//  Copyright (c) 2015 Marcos. All rights reserved.
+//
+
+#import "MyTableViewController.h"
+
+@interface MyTableViewController ()
+
+@end
+
+@implementation MyTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- pruebaTablas/ViewController.m
+++ pruebaTablas/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+NSMutableArray * scores;
+NSUInteger pos;
+
 @interface ViewController ()
 
 @end
@@ -17,11 +20,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    scores = [[NSMutableArray alloc] init];
+    
+    // Inicializamos losv alores del array
+    [scores addObject:@"001. 15/01/2014 15:00"];
+    [scores addObject:@"002. 15/01/2014 15:00"];
+    [scores addObject:@"003. 15/01/2014 15:00"];
+    [scores addObject:@"004. 15/01/2014 15:00"];
+    [scores addObject:@"005. 15/01/2014 15:00"];
+    [scores addObject:@"006. 15/01/2014 15:00"];
+    [scores addObject:@"007. 15/01/2014 15:00"];
+    [scores addObject:@"008. 15/01/2014 15:00"];
+    [scores addObject:@"009. 15/01/2014 15:00"];
+    [scores addObject:@"010. 15/01/2014 15:00"];
+    [scores addObject:@"011. 15/01/2014 15:00"];
+    [scores addObject:@"012. 15/01/2014 15:00"];
+    [scores addObject:@"013. 15/01/2014 15:00"];
+    [scores addObject:@"014. 15/01/2014 15:00"];
+    [scores addObject:@"015. 15/01/2014 15:00"];
+    
+    [scores addObject:@"057. 28/01/2014 15:00"];
+    
+    [scores sortUsingComparator:^NSComparisonResult(NSString *obj1, NSString *obj2) {
+        return -[obj1 compare:obj2 options:NSNumericSearch];
+    }];
+    pos = [scores indexOfObject: @"057. 28/01/2014 15:00"];
+
+    [self.myTable  setContentOffset:CGPointMake(0,( pos * 44 )) animated:YES];
+    [self.myTable reloadData];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [scores count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [scores objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
 
 @end
